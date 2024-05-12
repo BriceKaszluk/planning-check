f6428ec623f4839af3729d95a1b360deefa537c2
BriceKaszluk <kaszlukb@gmail.com>
2024-05-12 13:18:16 +0200

    feat(front): -add loading page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
+import { Suspense } from "react";
+import Loading from "./feed/loading";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -7,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Inside Water",
+  description: "Reprends le contrôle de ton planning",
 };
 
 export default function RootLayout({
@@ -17,10 +19,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={GeistSans.className}>
+    <html lang="fr" className={GeistSans.className}>
       <body className="bg-background text-foreground">
         <main className="min-h-screen flex flex-col items-center">
+        <Suspense fallback={<Loading />}>
           {children}
+        </Suspense>
         </main>
       </body>
     </html>
